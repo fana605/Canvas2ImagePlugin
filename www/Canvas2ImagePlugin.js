a475bdc547129940bc0ebe4d8d96effb837a8add
fana605 <fana605@yahoo.com>
2016-08-07 17:47:50 +0100

    Update Canvas2ImagePlugin.js

--- www/Canvas2ImagePlugin.js
+++ www/Canvas2ImagePlugin.js
@@ -22,7 +22,8 @@
         }
         else {
             var canvas = (typeof canvasId === "string") ? document.getElementById(canvasId) : canvasId;
-            var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
+            //var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
+            var imageData = canvas.toDataURL("image/jpeg", 1.0).replace(/data:image\/jpeg;base64,/,'');
             var extension='.png';
             var destQuality=100;
             if (fileExtension) extension=fileExtension.toLowerCase();
